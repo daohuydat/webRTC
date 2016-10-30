1fa92a1488eac3a51db6ed9c477bd88a68351032
= <thattinhkiemphai.07@gmail.com>
2016-10-30 20:36:09 +0700

    next

---
+++ js/client2.js
@@ -0,0 +1,26 @@
+var connection = new WebSocket('ws:https://daohuydat.github.io/webRTC/p2p.html');
+var name = "";
+
+var loginName = document.querySelector('#loginName');
+var loginBtn = document.querySelector('#loginBtn');
+var friendName = document.querySelector('#friendName');
+var connectBtn = document.querySelector('#connectFriendBtn');
+
+var connectedUser, myConnection;
+
+// user click login button
+loginBtn.addEventListener('click', function (e) {
+  name = loginName.value;
+
+  if(name.length > 0) {
+    send({
+      type: "login",
+      name: name
+    });
+  }
+});
+
+// handle message from server
+connection.onmessage = function (message) {
+  console.log("Got message", message.data);
+};
